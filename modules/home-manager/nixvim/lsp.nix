ff4314b6db25b8ee27cd136bd244736ff97581c2
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2025-01-11 13:07:58 +0100

    feat(lsp): enable additional language servers for HTML, CSS, JSON, YAML, Docker

--- modules/home-manager/nixvim/lsp.nix
+++ modules/home-manager/nixvim/lsp.nix
@@ -3,8 +3,21 @@
     enable = true;
 
     servers = {
-      nil_ls.enable = true;
-      ts_ls.enable = true;
+      nil_ls.enable = true; # Nix
+      ts_ls.enable = true; # TypeScript
+      pyright.enable = true; # Python
+      bashls.enable = true;
+      dockerls.enable = true;
+      docker_compose_language_service.enable = true;
+      html.enable = true;
+      cssls.enable = true;
+      jsonls.enable = true;
+      yamlls = {
+        enable = true;
+        settings.yaml.schemas = {
+          "https://json.schemastore.org/github-workflow.json" = "/.github/workflows/*";
+        };
+      };
     };
   };
 }
