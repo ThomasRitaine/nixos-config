754cd1b70feca5e34378f72ccbd0427a4b7aa786
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2025-01-11 13:08:23 +0100

    feat(nixvim): implement automatic formatting on save using autocommands

--- modules/home-manager/nixvim/plugins/conform.nix
+++ modules/home-manager/nixvim/plugins/conform.nix
@@ -18,24 +18,14 @@
           };
         };
 
-        format_on_save = ''
-          function(bufnr)
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-              return
-            end
-
-            if slow_format_filetypes[vim.bo[bufnr].filetype] then
-              return
-            end
-
-            local function on_format(err)
-              if err and err:match("timeout$") then
-                slow_format_filetypes[vim.bo[bufnr].filetype] = true
-              end
-            end
-
-            return { timeout_ms = 200, lsp_fallback = true }, on_format
-           end
+        luaConfig.post = ''
+          -- Create an autocommand for formatting on save
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+              require("conform").format({ bufnr = args.buf })
+            end,
+          })
         '';
       };
     };
