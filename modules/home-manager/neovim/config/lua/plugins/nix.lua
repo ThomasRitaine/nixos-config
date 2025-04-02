9fe768ca17ba23c4442f87188f00f7741094dff3
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2025-04-02 10:27:05 +0200

    build(neovim): ensure nixfmt is installed via mason.nvim

---
+++ modules/home-manager/neovim/config/lua/plugins/nix.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/conform.nvim",
+  dependencies = {
+    {
+      "mason.nvim",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        vim.list_extend(opts.ensure_installed, { "nixfmt" })
+      end,
+    },
+  },
+}
