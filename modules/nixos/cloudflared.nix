25ac738d7ea7ee51696aa0dc20ebe4771e9b32ff
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2025-04-18 16:14:02 +0200

    feat(cloudflared): enable Cloudflare tunnels configuration

--- modules/nixos/cloudflared.nix
+++ modules/nixos/cloudflared.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [ cloudflared ];
 
-  services.cloudflared.enable = true;
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
+      "21b52d0a-924c-4955-a10a-109eb3926a41" = {
+        credentialsFile = "/etc/nixos/secrets/cloudflare-tunnel-token.txt";
+        default = "http_status:404";
+      };
+    };
+  };
 }
