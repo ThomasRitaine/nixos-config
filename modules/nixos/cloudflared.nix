49d7b2d999ac5a14e84a4b03e9921042cc79cb0e
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2025-04-21 19:08:51 +0200

    feat(cloudflared): add new tunnel configuration with HTTPS and SSH services

--- modules/nixos/cloudflared.nix
+++ modules/nixos/cloudflared.nix
@@ -4,9 +4,19 @@
   services.cloudflared = {
     enable = true;
     tunnels = {
-      "21b52d0a-924c-4955-a10a-109eb3926a41" = {
-        credentialsFile = "/etc/nixos/secrets/cloudflare-tunnel-token.txt";
+      "771c0877-3149-40b3-bd89-7a94aab7b4d9" = {
+        credentialsFile =
+          "/etc/nixos/secrets/771c0877-3149-40b3-bd89-7a94aab7b4d9.json";
         default = "http_status:404";
+        ingress = {
+          "phoenix86-thomas.ritaine.com" = {
+            service = "https://localhost:443";
+            originRequest = { noTLSVerify = true; };
+          };
+          "ssh-phoenix86-thomas.ritaine.com" = {
+            service = "ssh://localhost:22";
+          };
+        };
       };
     };
   };
