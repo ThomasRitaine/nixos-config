c125485712c8d8ac40e5c304ea587f52496352ec
Thomas Ritaine <thomas.ritaine@outlook.com>
2025-10-20 23:43:11 +0200

    feat(wezterm): add GPU acceleration support using nixGLIntel

--- modules/home-manager/wezterm/wezterm.nix
+++ modules/home-manager/wezterm/wezterm.nix
@@ -1,4 +1,24 @@
-{
+{ pkgs, ... }: {
+
+  home.packages = with pkgs; [
+    nixgl.nixGLIntel
+    (pkgs.writeShellScriptBin "wezterm" ''
+      exec ${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel ${pkgs.wezterm}/bin/wezterm "$@"
+    '')
+  ];
+
   home.file.".config/wezterm/wezterm.lua".source = ./wezterm.lua;
   home.file.".config/wezterm/lua".source = ./lua;
+
+  # Add a desktop entry for the application launcher
+  xdg.desktopEntries.wezterm = {
+    name = "WezTerm";
+    genericName = "Terminal Emulator";
+    comment = "A GPU-accelerated cross-platform terminal emulator";
+    exec =
+      "${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel ${pkgs.wezterm}/bin/wezterm start --cwd .";
+    icon = "org.wezfurlong.wezterm";
+    categories = [ "System" "TerminalEmulator" "Utility" ];
+    terminal = false;
+  };
 }
