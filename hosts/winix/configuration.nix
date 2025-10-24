ccda4132ac32c1219e3b6e98f7305f2566cf701f
Thomas Ritaine <thomas.ritaine@outlook.com>
2025-10-24 17:36:23 +0200

    refactor(configuration): simplify system and package configurations

--- hosts/winix/configuration.nix
+++ hosts/winix/configuration.nix
@@ -1,82 +1,47 @@
-{
-  inputs,
-  ...
-}:
+{ inputs, ... }:
 let
-  # FOLLOW THE BELOW INSTRUCTIONS LINE BY LINE TO SET UP YOUR SYSTEM
-
-  # Package configuration - sets up package system with proper overlays
-  # Most users won't need to modify this section
   system = "x86_64-linux";
   pkgs = import inputs.nixpkgs {
     inherit system;
     config.allowUnfree = true;
-    overlays = [
-      inputs.hydenix.overlays.default
-    ];
+    overlays = [ inputs.hydenix.overlays.default ];
   };
-in
-{
+in {
   nixpkgs.pkgs = pkgs; # Set pkgs for hydenix globally
 
   imports = [
     # hydenix inputs - Required modules, don't modify unless you know what you're doing
     inputs.hydenix.inputs.home-manager.nixosModules.home-manager
     inputs.hydenix.nixosModules.default
-    ../../modules/hydenix/system # Your custom system modules
-    ./hardware-configuration.nix # Auto-generated hardware config
 
-    # Hardware Configuration - Uncomment lines that match your hardware
-    # Run `lshw -short` or `lspci` to identify your hardware
+    ./system.nix
+    ./hardware-configuration.nix
 
-    # GPU Configuration (choose one):
-    # inputs.nixos-hardware.nixosModules.common-gpu-nvidia # NVIDIA
-    # inputs.nixos-hardware.nixosModules.common-gpu-amd # AMD
-
-    # CPU Configuration (choose one):
-    # inputs.nixos-hardware.nixosModules.common-cpu-amd # AMD CPUs
     inputs.nixos-hardware.nixosModules.common-cpu-intel
-
-    # Additional Hardware Modules - Uncomment based on your system type:
-    # inputs.nixos-hardware.nixosModules.common-hidpi # High-DPI displays
-    inputs.nixos-hardware.nixosModules.common-pc-laptop # Laptops
-    # inputs.nixos-hardware.nixosModules.common-pc-ssd # SSD storage
+    inputs.nixos-hardware.nixosModules.common-pc-laptop
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
   ];
 
-
-  # Home Manager Configuration - manages user-specific configurations (dotfiles, themes, etc.)
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
     extraSpecialArgs = { inherit inputs; };
-    users."thomas" =
-      { ... }:
-      {
-        imports = [
-          inputs.hydenix.homeModules.default
-          ../../modules/hydenix/hm # Your custom home-manager modules (configure hydenix.hm here!)
-        ];
-      };
+    users."thomas" = { ... }: {
+      imports = [ inputs.hydenix.homeModules.default ./hm.nix ];
+    };
   };
 
   users.users.thomas = {
     isNormalUser = true;
-    # initialPassword = "hydenix";
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "video"
-    ];
+    extraGroups = [ "wheel" "networkmanager" "video" ];
     shell = pkgs.fish;
   };
 
-  # Hydenix Configuration - Main configuration for the Hydenix desktop environment
   hydenix = {
     enable = true;
     hostname = "winix";
     timezone = "Europe/Paris";
     locale = "en_US.UTF-8";
-    # For more configuration options, see: ./docs/options.md
   };
 
   # System Version - Don't change unless you know what you're doing (helps with system upgrades and compatibility)
