f2c623ab86b61194555c96f2ca4365d148674e49
Thomas Ritaine <thomas.ritaine@outlook.com>
2025-10-27 14:49:26 +0100

    fix(hyprland): center eDP-1 monitor horizontally under the above monitor

--- modules/home-manager/hyprland/monitors.nix
+++ modules/home-manager/hyprland/monitors.nix
@@ -3,7 +3,7 @@
     enable = true;
     overrideConfig = ''
       monitor = HDMI-A-1, 1920x1080@60, 0x0, 1.20
-      monitor = eDP-1, 1920x1080@60, 160x900, 1.20
+      monitor = eDP-1, 1920x1080@60, 0x900, 1.20
 
       workspace = HDMI-A-1, 2
       workspace = eDP-1, 1
