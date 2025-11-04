d9401868507409193b413d04b5aa72fb5ff2ad1f
Thomas Ritaine <thomas.ritaine@outlook.com>
2025-11-04 08:10:47 +0100

    feat(kitty): switch to CaskaydiaCove Nerd Font for improved icon rendering

--- hosts/winix/hm.nix
+++ hosts/winix/hm.nix
@@ -43,8 +43,8 @@
       font_size 12.0
       window_padding_width 0
 
-      font_family JetBrainsMono Nerd Font Mono
-      symbol_map U+E000-U+F8FF,U+F0000-U+FFFFF,U+100000-U+10ffff JetBrainsMono Nerd Font
+      # Use non mono font for nerd icons
+      symbol_map U+E5FA-U+E6AC,U+E700-U+E7C5,U+F000-U+F2E0,U+E200-U+E2A9,U+F0001-U+F1AF0,U+E300-U+E3E3,U+F400-U+F532,U+2665,U+26A1,U+E0A0-U+E0A2,U+E0B0-U+E0B3,U+E0A3,U+E0B4-U+E0C8,U+E0CA,U+E0CC-U+E0D4,U+23FB-U+23FE,U+2B58,U+F300-U+F32F,U+E000-U+E00A,U+EA60-U+EBEB CaskaydiaCove Nerd Font
     '';
 
     shell = {
