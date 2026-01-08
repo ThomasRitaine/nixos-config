3eaa8ffdb71dc19366d29befab1c2cfc19f062de
Thomas Ritaine <thomas.ritaine@outlook.com>
2026-01-08 18:12:20 +0000

    refactor(config): cleanup redundant zsh and zoxide configurations

--- modules/home-manager/zoxide.nix
+++ modules/home-manager/zoxide.nix
@@ -1,9 +1,7 @@
-{ pkgs, ... }: {
-  home.packages = [ pkgs.zoxide ];
-
+{ ... }:
+{
   programs.zoxide = {
     enable = true;
-    enableZshIntegration = true;
     options = [ "--cmd cd" ];
   };
 }
--- modules/nixos/zsh.nix
+++ modules/nixos/zsh.nix
@@ -1,6 +1,4 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ zsh ];
-
   programs.zsh.enable = true;
 
   users.defaultUserShell = pkgs.zsh;
