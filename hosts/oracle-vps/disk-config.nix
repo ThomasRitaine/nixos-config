4aa21bf831581348ae8b0fdf044acbaeba4ff1f3
Thomas Ritaine <thomas.ritaine@outlook.com>
2026-01-27 21:55:16 +0000

    feat(oracle-vps): migrate disk config to LVM to merge boot and block volumes

--- hosts/oracle-vps/disk-config.nix
+++ hosts/oracle-vps/disk-config.nix
@@ -1,6 +1,7 @@
 {
   disko.devices = {
     disk = {
+      # Disk 1 (Boot Volume): Holds the Bootloader + Part of the Storage Pool
       main = {
         type = "disk";
         device = "/dev/sda";
@@ -10,19 +11,54 @@
             boot = {
               size = "512M";
               type = "EF00";
+              priority = 1;
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
             };
-            root = {
+            lvm = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "lvm_pv";
+                vg = "pool";
               };
+            };
+          };
+        };
+      };
+
+      # Disk 2 (Block Volume): Entirely given to the Storage Pool
+      storage = {
+        type = "disk";
+        device = "/dev/sdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            lvm = {
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
+            };
+          };
+        };
+      };
+    };
+
+    # Combine disks into one root volume
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
             };
           };
         };
