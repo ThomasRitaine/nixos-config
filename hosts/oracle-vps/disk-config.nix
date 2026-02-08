4171212ba7d9e8d6e629475406bc8dc11ceee70d
Thomas Ritaine <thomas.ritaine@outlook.com>
2026-02-08 23:41:50 +0000

    refactor(oracle-vps): simplify disk layout to single boot volume disk and remove LVM

--- hosts/oracle-vps/disk-config.nix
+++ hosts/oracle-vps/disk-config.nix
@@ -1,7 +1,6 @@
 {
   disko.devices = {
     disk = {
-      # Disk 1 (Boot Volume): Holds the Bootloader + Part of the Storage Pool
       main = {
         type = "disk";
         device = "/dev/sda";
@@ -11,54 +10,19 @@
             boot = {
               size = "512M";
               type = "EF00";
-              priority = 1;
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
             };
-            lvm = {
+            root = {
               size = "100%";
               content = {
-                type = "lvm_pv";
-                vg = "pool";
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
-            };
-          };
-        };
-      };
-
-      # Disk 2 (Block Volume): Entirely given to the Storage Pool
-      storage = {
-        type = "disk";
-        device = "/dev/sdb";
-        content = {
-          type = "gpt";
-          partitions = {
-            lvm = {
-              size = "100%";
-              content = {
-                type = "lvm_pv";
-                vg = "pool";
-              };
-            };
-          };
-        };
-      };
-    };
-
-    # Combine disks into one root volume
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
             };
           };
         };
